46b6c2782afee51bfb466c84db5b35e746e7e80a
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2022-07-06 20:10:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const { v4: uuid } = require('uuid')
+
+class Timegate {
+
+  constructor(options = {}) {
+
+    this.name = options.name || uuid()
+    this.prefix = options.prefix || ''
+
+    this.meta = {}
+    this.series = {}
+
+    this.startTime = Date.now()
+    this.endTime = null
+  }
+
+  setData(key, value) {
+    this.meta[key] = value
+  }
+
+  getData(key) {
+    return this.meta[key]
+  }
+
+  start(name) {
+    this.name = name || this.name
+    this.startTime = Date.now()
+  }
+
+  stop() {
+    this.endTime = Date.now()
+
+    return {
+      name: `${this.prefix}-${this.name}`,
+      startTime: this.startTime,
+      endTime: this.endTime,
+      totalTime: this.endTime - this.startTime,
+      meta: this.meta,
+      series: Object.values(this.series)
+    }
+  }
+
+  gate(name) {
+    if (this.series[name]) throw new Error(`Gate ${name} already exists`)
+
+    this.series[name] = {
+      name: `${this.name}-${name}`,
+      startTime: Date.now(),
+      endTime: null,
+      sinceStart: Date.now() - this.startTime
+    }
+
+    return this.series[name]
+  }
+
+  gateEnd(name) {
+    if (!this.series[name]) throw new Error(`Gate ${name} does not exist`)
+
+    this.series[name].endTime = Date.now()
+
+    return this.series[name]
+  }
+}
+
+module.exports = Timegate
